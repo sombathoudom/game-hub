e24eeefb6bd75f84bbe10f1811442ab6fbb0ed50
Chheng Sombathoudom <sombathoudom.chheng@cogetel.com.kh>
2023-12-20 15:35:31 +0700

    refactor remove duplicated styled

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -4,22 +4,25 @@ import { Card, CardBody, HStack, Heading, Image, Text } from '@chakra-ui/react';
 import PlatformIconList from './PlatformIconList';
 import CriticScore from './CriticScore';
 import getCroppedImageUrl from '../services/image-url';
+import GameCardContainer from './GameCardContainer';
 interface Props {
     game: Game
 }
 
 function GameCard({game}: Props) {
   return (
-    <Card borderRadius={10} overflow='hidden' width={'300px'}>
-        <Image src={getCroppedImageUrl(game.background_image)}/>
-        <CardBody>
-            <Heading fontSize='2xl'>{game.name}</Heading>
-            <HStack justifyContent={'space-between'}>
-              <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)}/>
-              <CriticScore score={game.metacritic}/>
-            </HStack>
-        </CardBody>
-    </Card>
+    <GameCardContainer>
+      <Card>
+          <Image src={getCroppedImageUrl(game.background_image)}/>
+          <CardBody>
+              <Heading fontSize='2xl'>{game.name}</Heading>
+              <HStack justifyContent={'space-between'}>
+                <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)}/>
+                <CriticScore score={game.metacritic}/>
+              </HStack>
+          </CardBody>
+      </Card>
+    </GameCardContainer>
   )
 }
 
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -2,6 +2,7 @@ import { SimpleGrid, Text } from '@chakra-ui/react';
 import useGames from '../hooks/useGames';
 import GameCard from './GameCard';
 import GameCardSkeleton from './GameCardSkeleton';
+import GameCardContainer from './GameCardContainer';
 function GameGrid() {
     const {games, error, isLoading} = useGames();
     const skeletons = [1,2,3,4,5,6,7,8,9,10];
@@ -14,7 +15,10 @@ function GameGrid() {
                 lg:3,
                 xl:5
             }} padding={10} spacing={10}>
-                {isLoading && skeletons.map(skeleton => <GameCardSkeleton key={skeleton} />)}
+                {isLoading && skeletons.map((skeleton) => (
+                <GameCardContainer>
+                    <GameCardSkeleton key={skeleton} />
+                </GameCardContainer>))}
                 {games.map(game => 
                     <GameCard key={game.id} game={game}/>
                 )}
