3abcb534f0efc2cd00f77a19afd0592aaa5d046b
Chheng Sombathoudom <sombathoudom.chheng@cogetel.com.kh>
2023-12-21 10:19:19 +0700

    custom theme to darker

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,10 +1,11 @@
 import useData from "./useData"
-
+import platform from "../data/platform"
 export interface Platform {
     id: number
     name: string
     slug: string
 }
 
-const usePlatforms = () => useData<Platform>('/platforms/lists/parents');
+// const usePlatforms = () => useData<Platform>('/platforms/lists/parents');
+const usePlatforms = () => ({data:platform, isLoading:false, error:null});
 export default usePlatforms;
