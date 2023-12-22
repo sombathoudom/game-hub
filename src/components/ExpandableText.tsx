26de1492386d863e3d2d9eeb504087533010c1d8
Chheng Sombathoudom <sombathoudom.chheng@cogetel.com.kh>
2023-12-22 11:02:09 +0700

    build expandable text

---
+++ src/components/ExpandableText.tsx
@@ -0,0 +1,21 @@
+import { Button, Text } from '@chakra-ui/react';
+import React, { useState } from 'react'
+
+interface Props {
+    children: string;
+}
+function ExpandableText({children}: Props) {
+    const [expended, setExpanded] = useState(false);
+    const limit = 300;
+    if(!children) return null;
+    if(children.length <=limit) 
+        return <Text>{children}</Text>
+    const summary = expended ? children : children.substring(0,limit).concat('...');
+
+    return (
+        <Text>{summary} <Button size={'xs'} marginLeft={2} fontWeight={'bold'} colorScheme='yellow' 
+        onClick={() => setExpanded(!expended)}>{expended ? 'show less' : 'show more'}</Button></Text>
+    )
+}
+
+export default ExpandableText
