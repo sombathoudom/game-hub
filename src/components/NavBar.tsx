614221964aea47d24046163d3be6c20821f4206a
Chheng Sombathoudom <sombathoudom.chheng@cogetel.com.kh>
2023-12-25 11:03:43 +0700

    fix navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,11 +3,12 @@ import logo from '../assets/react.svg';
 import React from 'react'
 import ColorModeSwitch from './ColorModeSwitch';
 import SearhInput from './SearhInput';
+import { Link } from 'react-router-dom';
 
 function NavBar() {
   return (
     <HStack padding="10px">
-        <Image src={logo} alt="Vercel Logo" boxSize="60px"  />
+        <Link to={"/"}><Image src={logo} alt="Vercel Logo" boxSize="60px"  /></Link>
         <SearhInput/>
         <ColorModeSwitch/>
     </HStack>
--- src/components/SearhInput.tsx
+++ src/components/SearhInput.tsx
@@ -2,15 +2,20 @@ import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react'
 import { useRef } from 'react';
 import { BsSearch } from 'react-icons/bs';
 import useGameQueryStorer from '../store';
+import { useNavigate } from 'react-router-dom';
 
 
 function SearhInput() {
   const ref = useRef<HTMLInputElement>(null);
-  const setSearchText = useGameQueryStorer(s => s.setSearchText);
+  const setSearchText = useGameQueryStorer(s => s.setSearchText); 
+  const navigate  = useNavigate();
   return (
     <form onSubmit={(e) => {
       e.preventDefault();
-      if(ref.current) setSearchText(ref.current.value);
+      if(ref.current)  {
+        setSearchText(ref.current.value);
+        navigate('/');
+      }
     }}>
       <InputGroup>
           <InputLeftElement children={<BsSearch/>} />
